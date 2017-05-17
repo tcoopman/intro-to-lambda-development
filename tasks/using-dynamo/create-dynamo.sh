8777aa2bc83dadbde3b9a5111c0c36d535273230
Thomas Coopman <thomas.coopman@gmail.com>
2017-05-17 12:34:04 +0200

    in region us-east-1

--- tasks/using-dynamo/create-dynamo.sh
+++ tasks/using-dynamo/create-dynamo.sh
@@ -1,4 +1,4 @@
-aws dynamodb create-table --table-name dynamo-test \
+aws dynamodb create-table --table-name dynamo-test --region us-east-1 \
   --attribute-definitions AttributeName=userid,AttributeType=S \
   --key-schema AttributeName=userid,KeyType=HASH \
   --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
